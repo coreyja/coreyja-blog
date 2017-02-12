04ec170fff322f84234928806cca244b795e275c
Corey Alexander <coreyja@gmail.com>
2017-02-12 15:38:18 -0500

    Set up BlogLayout so that all blog pages can share the same strucutre but not everything has to

--- config.rb
+++ config.rb
@@ -29,7 +29,7 @@ activate :blog do |blog|
   # Matcher for blog source files
   blog.sources = '{year}-{month}-{day}-{title}.html'
   blog.taglink = 'tags/{tag}.html'
-  blog.layout = 'layout'
+  blog.layout = 'blog_layout'
   blog.summary_separator = /(READMORE)/
   blog.summary_length = 250
   blog.year_link = '{year}.html'
@@ -47,10 +47,6 @@ activate :blog do |blog|
 end
 
 page '/feed.xml', layout: false
-# Reload the browser automatically whenever files change
-configure :development do
-  activate :livereload
-end
 
 # Methods defined in the helpers block are available in templates
 # helpers do
