700b179ed81039817a053e7c0df7e3d3e84e293e
Corey Alexander <coreyja@gmail.com>
2018-11-06 21:39:58 -0500

    Move JS to its own file

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -1,1 +1,24 @@
-// This is where it all goes :)
+function colorHex(color) {
+  return JSON.parse(document.querySelector('[name="theme-color-map-hex"]').content)[color];
+}
+function colorManifest(color) {
+  return JSON.parse(document.querySelector('[name="theme-color-manifest-map"]').content)[color];
+}
+
+function changeColorTo(color) {
+  var elementsToChange = document.querySelectorAll('[data-color-changer-class]');
+  Array.from(elementsToChange).forEach(function(element) {
+    element.className = element.dataset.colorChangerClass + color;
+  });
+  document.querySelector('[name="theme-color"]').content = '#' + colorHex(color);
+  document.querySelector('link[rel="manifest"]').href = colorManifest(color);
+}
+
+function changeThemeColor(event) {
+  changeColorTo(event.srcElement.dataset.color);
+}
+
+window.onload = function() {
+  var bars = document.getElementsByClassName('Header-coloredBar');
+  Array.from(bars).forEach(function(bar) { bar.onclick = changeThemeColor; })
+}
