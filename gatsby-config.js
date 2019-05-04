f27886969b4563746f47edd10aef8e7f60420fb2
Corey Alexander <coreyja@gmail.com>
2019-05-04 18:31:43 -0400

    Change favicons to mine

--- gatsby-config.js
+++ gatsby-config.js
@@ -64,7 +64,7 @@ module.exports = {
         background_color: `#aa66cc`,
         theme_color: `#aa66cc`,
         display: `minimal-ui`,
-        icon: `content/assets/gatsby-icon.png`,
+        icon: `content/assets/favicon.svg`,
       },
     },
     `gatsby-plugin-offline`,
