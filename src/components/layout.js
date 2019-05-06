0e21f1144029f609ef3a9451e8deba00f27bce39
Corey Alexander <coreyja@gmail.com>
2019-05-05 23:21:46 -0400

    cleanup

--- src/components/layout.js
+++ src/components/layout.js
@@ -13,10 +13,6 @@ class Layout extends React.Component {
     return (
       <div
         style={{
-          marginLeft: `auto`,
-          marginRight: `auto`,
-          // maxWidth: rhythm(24),
-          // padding: `${rhythm(1.5)} ${rhythm(3 / 4)}`,
           WebkitFontSmoothing: 'antialiased',
 					MozOsxFontSmoothing: 'grayscale',
         }}
