829b2542f8ae8c4b6b350dec98c26151e13e3827
Corey Alexander <coreyja@gmail.com>
2019-05-11 22:36:24 -0400

    Inline the Dracula theme from the archived repo so I can customize

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -3,6 +3,5 @@ import 'typeface-hack-nerd-font'
 import 'typeface-alternate-gothic1'
 import 'typeface-oswald'
 
-import 'gatsby-prismjs-dracula'
-
 import './src/styles/global.scss'
+import './src/styles/code.scss'
--- src/templates/year.js
+++ src/templates/year.js
@@ -1,4 +1,5 @@
-import { graphql, React, Link } from 'gatsby'
+import { graphql, Link } from 'gatsby'
+import React from 'react'
 
 import BlogLayout from '../components/blogLayout'
 import PostCards from '../components/postCards'
