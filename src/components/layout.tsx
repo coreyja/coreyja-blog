90b2b17b8207e091a3c2c410c08ec5d1847b83f4
Corey Alexander <coreyja@gmail.com>
2020-08-08 17:33:15 -0400

    Ignore one more ts

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -10,6 +10,7 @@ class Layout extends React.Component {
     return (
       <div
         style={{
+          // @ts-ignore 2322: This is a real prop but TS disagrees
           WebkitFontSmoothing: "antialiased",
           MozOsxFontSmoothing: "grayscale"
         }}
